8593e975871ef5b1637e9e06c9c14715e3d913af
RohithKalisetti13 <rohithkalisetti.13@gmail.com>
2026-09-01 21:20:20 -0700

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+         int maxW=0;
+         int totalW = 0;
+         int minload = 0;
+         for(int i: weights){
+            totalW += i;
+            maxW = max(maxW, i);
+         }
+         
+         int left = maxW;
+         int right = totalW;
+
+         while(left<=right){
+            int mid = left + (right-left)/2;
+            int daysneeded=1;
+            int currentload=0;
+
+            for(int i : weights){
+               if(currentload+i > mid){
+                  daysneeded++;
+                  currentload =0;
+               }
+               currentload+=i;
+            }
+            if(daysneeded<=days){
+               right=mid-1;
+            }
+            else{
+               left=mid+1;
+            }
+         }
+         return left;
+    }
+};
